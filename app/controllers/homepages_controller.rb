efc6fe4d3f989d8cf42762e38581de8c5522f75e
Dan Roberts <dan@adadevelopersacademy.org>
2016-10-31 16:57:03 -0700

    Clean up the views and controllers a bit.

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -9,7 +9,13 @@ class HomepagesController < ApplicationController
   end
 
   def create
-    Channel.by_name(params[:channel]).sendmsg(params[:message])
+    result = Channel.by_name(params[:channel]).sendmsg(params[:message])
+    if result["ok"]
+      flash[:notice] = "Successfully sent message to #{params[:channel]}"
+    else
+      flash[:notice] = "Failed to send message to #{params[:channel]}: #{result["error"]}"
+    end
+    redirect_to homepages_index_path
   end
 
   def new
