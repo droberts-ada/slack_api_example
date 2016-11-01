b386bd8d7e3de793ec56d2b0243fe2245e37a8db
Dan Roberts <dan@adadevelopersacademy.org>
2016-10-31 20:59:51 -0700

    Remove all Channel functionality in preperation for TDD

--- lib/channel.rb
+++ lib/channel.rb
@@ -3,19 +3,17 @@ class Channel
   attr_reader :name, :id, :purpose, :is_archived, :members
 
   def initialize(name, id, options = {} )
-    raise ArgumentError if name == nil || name == "" || id == nil || id == ""
-
-    @name = name
-    @id = id
-
-    @purpose = options[:purpose]
-    @is_archived = options[:is_archived]
-    @is_general = options[:is_archived]
-    @members = options[:members]
+    # Commented out because there's no way I'd be able to remember
+    # all this live.
+    # @purpose = options[:purpose]
+    # @is_archived = options[:is_archived]
+    # @is_general = options[:is_archived]
+    # @members = options[:members]
   end
 
+  # Send a message to this slack channel
+  # Returns the data from the Slack server's response
   def sendmsg(msg)
-    SlackApiWrapper.sendmsg(@name, msg)
   end
 
   # Create a class-level instance variable.
@@ -25,23 +23,21 @@ class Channel
     attr_reader :channels
   end
 
+  # Return a memoized set of all channels
   def self.all
-    @channels ||= SlackApiWrapper.listchannels
   end
 
+  # Foreget all memoized values
   def self.reset
-    @channels = nil
   end
 
   # Return either the first (probably only) channel matching
   # the given name, or nil.
   def self.by_name(name)
-    all.select{ |c| c.name == name }.first
   end
 
   # Return either the first (probably only) channel matching
   # the given ID, or nil.
   def self.by_id(id)
-    all.select{ |c| c.id == id }.first
   end
 end
